fcee990b7c8bb83ec92e159519978ddb853ae932
Siobahn <siobahnschuck@gmail.com>
2021-02-08 13:19:10 -0600

    TypeError fixed by seperating scripts

---
+++ main.js
@@ -0,0 +1,5 @@
+const playBtn = document.getElementById('play-btn')
+
+playBtn.addEventListener('click', () => {
+  location.href = 'draw.html'
+})
--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 console.log('hello world')
 
-const play = document.querySelector('.play-btn')
-
-play.addEventListener('click', () => {
-  window.location.href = 'draw.html'
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed')
 })
+
+const canvas = document.getElementById('paint')
+const instructions = document.querySelector('.instructions')
+
+let ctx = canvas.getContext('2d')
+console.log(ctx)
+let pos = { x: 0, y: 0 }
+
+const setPos = (e) => {
+  pos.x = e.clientX
+  pos.y = e.clientY
+}
+
+const draw = (e) => {
+  ctx.beginPath()
+  ctx.lineWidth = 5
+  ctx.lineCap = 'round'
+  // ctx.strokeStyle =
+
+  ctx.moveTo(pos.x, pos.y)
+  ctx.lineTo(pos.x, pos.y)
+
+  ctx.stroke()
+}
+
+canvas.addEventListener('click', draw)
+canvas.addEventListener('mousedown', setPos)
+canvas.addEventListener('mouseenter', setPos)
